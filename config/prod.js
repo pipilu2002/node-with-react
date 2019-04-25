9df36b1df741aa1d95c32beecc6a79f0f613dd56
pipilu2002 <tutran985@gmail.com>
2019-04-25 11:20:31 +0700

    thay doi prod nhu dev

--- config/prod.js
+++ config/prod.js
@@ -7,9 +7,17 @@
 // }
 
 // nhuwnxg cái này sẽ được thêm trên heroku https://dashboard.heroku.com/apps/glacial-tor-67386/settings   => Reveal Config Vars
+// module.exports = {
+// 	googleClientID 		: process.env.GOOGLE_CLIENT_ID,
+// 	googleClientSecret 	: process.env.GOOGLE_CLIENT_SECRET,
+// 	mongoURI			: process.env.MONGO_URI,
+// 	cookieKey 			: process.env.COOKIE_KEY,
+// }
+
+
 module.exports = {
-	googleClientID 		: process.env.GOOGLE_CLIENT_ID,
-	googleClientSecret 	: process.env.GOOGLE_CLIENT_SECRET,
-	mongoURI			: process.env.MONGO_URI,
-	cookieKey 			: process.env.COOKIE_KEY,
+	googleClientID 		: '552942370964-cv9k9jpohpf5idpemooloit6bt9i6g6u.apps.googleusercontent.com',
+	googleClientSecret 	: '9JlgEoM0EF_0mJHtcd1SRQZG',
+	mongoURI			: 'mongodb+srv://tutran197:10101997@cluster0-ayun3.mongodb.net/test?retryWrites=true',
+	cookieKey 			: 'đây là chuỗi cookieKey mà tao đã tạo',
 }
